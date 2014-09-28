bf5dec90020975ea63031e606f7ee79a9ed2694c
Matias Ibañez <mibanez@antica.cl>
2014-09-28 11:07:00 -0300

    Testing MetricEventListener notifications Plus some refactoring of the test class

--- src/test/java/cl/niclabs/scada/acs/component/controllers/monitor/MonitorControllerImplTest.java
+++ src/test/java/cl/niclabs/scada/acs/component/controllers/monitor/MonitorControllerImplTest.java
@@ -27,21 +27,42 @@ public class MonitorControllerImplTest {
         public void measure(RecordStore store) { counter++; }
         public String getValue() { return "foo-" + counter; }
     }
+    public static class FooMetricEventListener implements MetricEventListener {
+        public String value, name, clazzName;
+        public void notifyUpdate(MetricEvent event) {
+            name = event.getMetricName();
+            value = event.getMetricValue().toString();
+            clazzName = event.getMetricClass().getName();
+        }
+    }
 
     @Test
     @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
     public void metrics() {
 
-        MonitorControllerImpl monitorController = getSimpleMonitor();
-        if (monitorController == null) {
-            fail("Fail when creating the MonitorControllerImpl");
+        MonitorControllerImpl monitorController = new MonitorControllerImpl();
+
+        try {
+            MetricEventListener metricEventListener = mock(MetricEventListener.class);
+            doNothing().when(metricEventListener).notifyUpdate(any(MetricEvent.class));
+            monitorController.bindFc(MonitorControllerImpl.METRIC_EVENT_LISTENER_CLIENT_ITF, metricEventListener);
+        }
+        catch (NoSuchInterfaceException e) {
+            fail("Fail when creating the MonitorControllerImpl: " + e.getMessage());
         }
 
-        Wrapper<Boolean> wrapper = monitorController.addMetric("foo", FooMetric.class.getName());
-        System.out.print("AddMetric result = " + wrapper.getMessage());
-        if (wrapper.getException() != null) {
-            wrapper.getException().printStackTrace();
-        }
+        // wrong class path
+        Wrapper<Boolean> wrapper = monitorController.addMetric("foo", "not.a.real.path.metric");
+        assertEquals(false, wrapper.unwrap());
+        assertNotNull(wrapper.getException());
+
+        // class is not a metric
+        wrapper = monitorController.addMetric("foo", FakeMetric.class.getName());
+        assertEquals(false, wrapper.unwrap());
+        assertNull(wrapper.getException());
+
+        // a correct one
+        wrapper = monitorController.addMetric("foo", FooMetric.class.getName());
         assertTrue(wrapper.unwrap());
         assertTrue(monitorController.getValue("foo").unwrap().equals("foo-0"));
         assertTrue(monitorController.measure("foo").unwrap().equals("foo-1"));
@@ -49,59 +70,44 @@ public class MonitorControllerImplTest {
         assertTrue(monitorController.measure("foo").unwrap().equals("foo-3"));
         assertTrue(monitorController.getValue("foo").unwrap().equals("foo-3"));
 
+        // bad value cast
         try {
             Wrapper<Integer> fake = monitorController.getValue("foo");
             @SuppressWarnings("UnusedAssignment") int x = fake.unwrap();
             fail("ClassCastException expected");
-        } catch (ClassCastException e) {
-            // ok
-        }
+        } catch (ClassCastException ignored) {}
 
+        // add multiple metrics
         assertTrue(monitorController.addMetric("foo2", FooMetric.class).unwrap());
         assertTrue(monitorController.addMetric("foo3", FooMetric.class).unwrap());
 
         HashSet<String> nameSet = new HashSet<>();
         Collections.addAll(nameSet, monitorController.getMetricNames().unwrap());
-
         assertTrue(nameSet.contains("foo"));
         assertTrue(nameSet.contains("foo2"));
         assertTrue(nameSet.contains("foo3"));
 
+        // removing a metric
         assertTrue(monitorController.removeMetric("foo2").unwrap());
 
         nameSet.clear();
         Collections.addAll(nameSet, monitorController.getMetricNames().unwrap());
-
         assertTrue(nameSet.contains("foo"));
         assertFalse(nameSet.contains("foo2"));
         assertTrue(nameSet.contains("foo3"));
     }
 
     @Test
-    public void wrongMetricInstantiations() {
+    public void subscriptions() {
 
-        MonitorControllerImpl monitorController = getSimpleMonitor();
-        if (monitorController == null) {
-            fail("Fail when creating the MonitorControllerImpl");
-        }
+        MonitorControllerImpl monitorController = new MonitorControllerImpl();
+        FooMetricEventListener metricEventListener = new FooMetricEventListener();
 
-        // wrong path
-        Wrapper<Boolean> wrapper = monitorController.addMetric("foo", "not.a.real.path.metric");
-        assertEquals(false, wrapper.unwrap());
-        assertNotNull(wrapper.getException());
-
-        // not a metric
-        wrapper = monitorController.addMetric("foo", FakeMetric.class.getName());
-        assertEquals(false, wrapper.unwrap());
-        assertNull(wrapper.getException());
-    }
-
-    @Test
-    public void metricSubscription() {
-
-        MonitorControllerImpl monitorController = getSimpleMonitor();
-        if (monitorController == null) {
-            fail("Fail when creating the MonitorControllerImpl");
+        try {
+            monitorController.bindFc(MonitorControllerImpl.METRIC_EVENT_LISTENER_CLIENT_ITF, metricEventListener);
+        } catch (NoSuchInterfaceException e) {
+            e.printStackTrace();
+            fail();
         }
 
         monitorController.addMetric("foo", FooMetric.class);
@@ -112,42 +118,9 @@ public class MonitorControllerImplTest {
         monitorController.notifyACSEvent(ACSEventType.VOID_REQUEST_SENT);
 
         assertEquals("foo-3", monitorController.getValue("foo").unwrap());
+        assertEquals("foo", metricEventListener.name);
+        assertEquals("foo-3", metricEventListener.value);
+        assertEquals(FooMetric.class.getName(), metricEventListener.clazzName);
     }
 
-    @Test
-    public void metricEventListener() {
-
-        MonitorControllerImpl monitorController = new MonitorControllerImpl();
-
-        try {
-            MetricEventListener metricEventListener = new MetricEventListener() {
-                @Override
-                public void notifyUpdate(MetricEvent event) {
-                    assertTrue(true);
-                }
-            };
-            monitorController.bindFc(MonitorControllerImpl.METRIC_EVENT_LISTENER_CLIENT_ITF, metricEventListener);
-        } catch (NoSuchInterfaceException e) {
-            e.printStackTrace();
-            fail();
-        }
-
-
-    }
-
-    private MonitorControllerImpl getSimpleMonitor() {
-        try {
-            MetricEventListener metricEventListener = mock(MetricEventListener.class);
-            doNothing().when(metricEventListener).notifyUpdate(any(MetricEvent.class));
-
-            MonitorControllerImpl monitorController = new MonitorControllerImpl();
-            monitorController.bindFc(MonitorControllerImpl.METRIC_EVENT_LISTENER_CLIENT_ITF, metricEventListener);
-
-            return monitorController;
-        }
-        catch (NoSuchInterfaceException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
